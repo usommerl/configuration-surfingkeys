130d941bf92df03f55bc8f20e6bd90c5eeb306ad
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2019-07-03 23:10:57 +0200

    Add theme definition

--- config.js
+++ config.js
@@ -1,3 +1,41 @@
 unmapAllExcept(['E', 'R', 'S', 'D', '<<', '>>', '<Ctrl-6>'], /(google|twitter)\.com/i);
 
 unmap('<Ctrl-h>')
+
+settings.theme = `
+.sk_theme {
+    font-family: sans-serif;
+    font-size: 11pt;
+    background: #24272e;
+    color: #abb2bf;
+}
+.sk_theme tbody {
+    color: #fff;
+}
+.sk_theme input {
+    color: #d0d0d0;
+}
+.sk_theme .url {
+    color: #61afef;
+}
+.sk_theme .annotation {
+    color: #56b6c2;
+}
+.sk_theme .omnibar_highlight {
+    color: #528bff;
+}
+.sk_theme .omnibar_timestamp {
+    color: #e5c07b;
+}
+.sk_theme .omnibar_visitcount {
+    color: #98c379;
+}
+.sk_theme #sk_omnibarSearchResult>ul>li:nth-child(odd) {
+    background: #303030;
+}
+.sk_theme #sk_omnibarSearchResult>ul>li.focused {
+    background: #3e4452;
+}
+#sk_status, #sk_find {
+    font-size: 20pt;
+}`;
