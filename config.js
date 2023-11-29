fc20ad3013c836c997a230082cbbe62e2b270b0d
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-11-29 11:02:31 +0100

    Unmap keys for another Grafana instance

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ api.unmapAllExcept(keySet2, /(twitter|reddit)\.com/i);
 api.unmapAllExcept(keySet2, /exelonix\.atlassian\.net/i);
 api.unmapAllExcept(keySet0, /(dev\.)?(monitoring|obre)\.exelonix\.com/i);
 api.unmapAllExcept(keySet0, /.+\.grafana\.net/i);
+api.unmapAllExcept(keySet0, /grafana\.waldwaechter\.de/i);
 api.unmapAllExcept(keySet0, /(ops)\.corona-warn-buzzer\.(de|com)/i);
 api.unmapAllExcept(keySet2, /bitbucket\.org/i);
 api.unmapAllExcept(keySet1, /github\.com/i);
